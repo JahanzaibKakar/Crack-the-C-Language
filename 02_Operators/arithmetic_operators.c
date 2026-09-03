f64faf83c92d227d30c6e4e85cebe93a5e9ed1f5
JahanzaibKakar <jahanzaibkakar706@gmail.com>
2026-09-04 01:01:05 +0200

    Practice arithmetic operators

---
+++ 02_Operators/arithmetic_operators.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 10;
+    int b = 5;
+
+    printf("Sum = %d\n", a + b);
+ printf("Sub = %d\n", a - b);
+ printf("Division = %d\n", a / b);
+ printf("Remainder = %d\n", a % b);
+    return 0;
+}
